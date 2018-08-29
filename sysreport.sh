4555d8ad8986c89ea0d6ac9be26cbcd4eaab1e7f
localadmin <eaton.chrisc@gmail.com>
2018-08-29 11:49:53 -0500

    Trying -e for newline

--- sysreport.sh
+++ sysreport.sh
@@ -10,7 +10,7 @@ myproc=$( nproc )
 
 myfile=report_$(date -I).log
 
-echo $hostvar'\n' >> $myfile
-echo $myip'\n' >> $myfile
-echo $mymem'\n' >> $myfile
-echo $myproc'\n' >> $myfile
+echo -e $hostvar >> $myfile
+echo -e $myip >> $myfile
+echo -e $mymem >> $myfile
+echo -e $myproc >> $myfile
